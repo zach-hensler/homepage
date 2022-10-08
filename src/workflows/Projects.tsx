68f2884e8bebd7b322305e6b3d67e02cbf82ce08
Zach Hensler <zhensler12@gmail.com>
2022-10-08 17:11:24 -0400

    feature/Add-card-component

---
+++ src/workflows/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Projects: React.FC = () => {
+    return (
+        <div>
+            <h2>Projects</h2>
+            <ul>
+                <li>View the code for <a href="https://github.com/zach-hensler/homepage" rel="noreferrer noopener" target="_blank">this project on github</a></li>
+            </ul>
+        </div>
+    )
+};
